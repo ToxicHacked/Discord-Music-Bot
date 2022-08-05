d126cec13cd70f39e3d28836a86be030539d7a1c
Ans Ahmed <96832336+ToxicHacked@users.noreply.github.com>
2022-08-05 07:44:16 -0700

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,4 @@
+@echo off 
+title Music Bot v2 Made By Made By Ahad and Ans Ahmed Tariq
+:START 
+npm install
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off 
+title Music Bot v2 Made By Made By Ahad and Ans Ahmed Tariq
+:START 
+node index.js
+goto START
